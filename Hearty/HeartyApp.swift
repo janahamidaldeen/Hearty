16b4185f80c12c3b149f0fa8ea90559ad85b89ee
Jana Hamidaldeen <janahamidaldeen@gmail.com>
2025-02-08 16:33:25 -0500

    Initial Commit

---
+++ Hearty/HeartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartyApp.swift
+//  Hearty
+//
+//  Created by Jana Hamidaldeen on 2025-02-08.
+//
+
+import SwiftUI
+
+@main
+struct HeartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HeartyTests/HeartyTests.swift
@@ -0,0 +1,17 @@
+//
+//  HeartyTests.swift
+//  HeartyTests
+//
+//  Created by Jana Hamidaldeen on 2025-02-08.
+//
+
+import Testing
+@testable import Hearty
+
+struct HeartyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
